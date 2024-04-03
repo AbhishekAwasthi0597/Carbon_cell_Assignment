ca94ef367e360453820429ad48ffefa40243d470
Abhishek Awasthi <114220502+AbhishekAwasthi0597@users.noreply.github.com>
2024-04-03 16:38:00 +0530

    Update UserManagementApplication.java

--- Downloads/userManagement/src/main/java/com/userlogin/userManagement/UserManagementApplication.java
+++ Downloads/userManagement/src/main/java/com/userlogin/userManagement/UserManagementApplication.java
@@ -5,7 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UserManagementApplication {
-
+@PostConstruct
+public void initLogin(){
+	List<AdminLoginEntity> lgn= Stream.of(
+			new AdminLoginEntity(1,"abhishekawasthi0597@gmail.com","1234"),
+	        new AdminLoginEntity(2,"ankitawasthi0597@gmail.com","12345")
+	).collect(Collectors.toList());
+	adminLoginRepo.saveAll(lgn);
+}
 	public static void main(String[] args) {
 		SpringApplication.run(UserManagementApplication.class, args);
 	}
